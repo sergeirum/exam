85adc7139b7d8ae98e51807f2ed3df767578c556
Sergei Rumiantsev <147652809+sergeirum@users.noreply.github.com>
2023-12-24 11:33:02 +0300

    all answers

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-export function multiply(a,b) {
+ function multiply(a,b) {
  return a*b   
+}
+
+function getRandom(num) {
+return Math.floor(Math.random()*num);
+}
+
+
+function convertToString(num) {
+if(isNaN(num)){
+    return 'NaN'
+}
+else{
+    if(num === 42){
+        return'the meaning of life and the universe'
+    }
+    else {
+        return num.toString()
+    }
+}
+}
+
+
+function reverse(text) {
+   return  text.split("").reverse().join("")
+}
+
+
+function countSyllables(text){
+if (!text){
+    return 0
+}
+
+return text.split("-").length
+}
+
+export {
+    multiply,getRandom,convertToString,reverse,countSyllables
 }
